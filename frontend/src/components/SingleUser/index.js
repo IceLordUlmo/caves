ea186f8ce0e3ddd353a44fa1b1e6e83c7252067e
IceLordUlmo <torgall22@gmail.com>
2023-05-25 21:36:17 -0500

    leaderboard stuff

--- frontend/src/components/SingleUser/index.js
+++ frontend/src/components/SingleUser/index.js
@@ -1,11 +1,21 @@
 import './SingleUser.css'
 
-export default function SingleUser(user) {
+export default function SingleUser(user)
+{
     user = user.user;
+    let classTag = '';
+    if (user.index < 3)
+    {
+        classTag = `champion${user.index + 1}`
+    }
     return (
-        <div>
-            {user.index}. {user.username}
-            Kills: {user.killCount}
+        <div className={classTag}>
+            <div className='single-user-name'>
+                {user.index}. {user.username}
+            </div>
+            <div className='single-user-kill-count'>
+                Kills: {user.killCount}
+            </div>
         </div>
     )
 }
